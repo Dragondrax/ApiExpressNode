7d65a62bd8b32ea7fa7d895b049fa50eb050fad1
Igor Hebling <igor.sallo@hotmail.com>
2021-09-21 23:24:03 -0300

    rotas

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const useRoutes = require('./routes/useRoutes');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+useRoutes(app)
+
+app.get('/', (req, res) => {
+    res.send('Hello World, pelo express')
+})
+
+app.listen(port, () => console.log('listening on port ' + port));
